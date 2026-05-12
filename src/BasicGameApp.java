4e4b6d92208dbd36cd4b8a3b72ab42a41ca4ad65
maxsimonson <max_simonson28@milton.edu>
2026-05-12 07:28:07 -0400

    HW

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -5,7 +5,16 @@ public class BasicGameApp {
     private Dealer d;
 
     public static void main(String[] args) {
-        
+        new BasicGameApp();
+    }
+
+    public BasicGameApp(){
+        System.out.println("Welcome to Blackjack");
+        deck = new Card[52];
+        for(int x = 0; x < 14; x++){
+            deck[x] = new Card(10,"hearts",x);
+            deck[x].printInfo();
+        }
     }
 
     public void compare(){
--- src/Card.java
+++ src/Card.java
@@ -4,4 +4,68 @@ public String suit;
 public int value;
 public String name;
 
+
+
+public Card(int pValue,String pSuit, int pName){
+    suit = pSuit;
+    value = pValue;
+    if(pName == 0){
+        name = "Ace";
+        value = 11;
+    }
+    else if(pName == 1){
+        name = "2";
+        value = 2;
+    }
+    else if(pName == 2){
+        name = "3";
+        value = 3;
+    }
+    else if(pName == 3){
+        name = "4";
+        value = 4;
+    }
+    else if(pName == 4){
+        name = "5";
+        value = 5;
+    }
+    else if(pName == 5){
+        name = "6";
+        value = 6;
+    }
+    else if(pName == 6){
+        name = "7";
+        value = 7;
+    }
+    else if(pName == 7){
+        name = "8";
+        value = 8;
+    }
+    else if(pName == 8){
+        name = "9";
+        value = 9;
+    }
+    else if(pName == 9){
+        name = "10";
+        value = 10;
+    }
+    else if(pName == 10){
+        name = "Prince";
+        value = 10;
+    }
+    else if(pName == 11){
+        name = "Queen";
+        value = 10;
+    }
+    else if(pName == 12){
+        name = "King";
+        value = 10;
+    }
+}
+
+public void printInfo(){
+    System.out.println("the " + name + " of " + suit + " is worth " + value + " points");
+}
+
+
 }
